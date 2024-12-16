dc0d294c9a9f950d8b8c5eb1de4f9dbc5ae558e9
TasyaTR17 <tasyaregina441@gmail.com>
2024-12-17 00:10:53 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,10 @@
 import json
 import zipfile
-import geopandas
 import numpy as np
 import pandas as pd
 import streamlit as st
 import datetime
 
-import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
